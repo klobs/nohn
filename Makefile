3a2557714bf1ec07ec3876f79c11ae7fcc91fc56
klObs <jw@klobs.de>
2017-02-02 23:28:19 +0100

    initial

---
+++ Makefile
@@ -0,0 +1,10 @@
+PROJECT = nohn
+PROJECT_DESCRIPTION = No Old Hacker News
+PROJECT_VERSION = 0.1.0
+
+DEPS = cowboy jiffy
+dep_cowboy_commit = master
+
+DEP_PLUGINS = cowboy
+
+include ../cowboy/erlang.mk
---
+++ src/nohn_fetcher.erl
@@ -0,0 +1,116 @@
+-module(nohn_fetcher).
+
+-behaviour(gen_server).
+
+%% API
+-export([start_link/0]).
+
+%% gen_server callbacks
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2,
+         terminate/2, code_change/3]).
+
+-export([refresh/1]).
+
+-record(state, {last_update,
+				hn_list=[]}
+		).
+
+-define(SERVER, ?MODULE).
+%%-define(FETCHINTERVAL, 300000).
+-define(FETCHINTERVAL, 3000).
+-define(HNURL, "https://hacker-news.firebaseio.com/v0/topstories.json").
+
+%%====================================================================
+%% API
+%%====================================================================
+%%--------------------------------------------------------------------
+%% Function: start_link() -> {ok,Pid} | ignore | {error,Error}
+%% Description: Starts the server
+%%--------------------------------------------------------------------
+start_link() ->
+  gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+
+%%====================================================================
+%% gen_server callbacks
+%%====================================================================
+
+%%--------------------------------------------------------------------
+%% Function: init(Args) -> {ok, State} |
+%%                         {ok, State, Timeout} |
+%%                         ignore               |
+%%                         {stop, Reason}
+%% Description: Initiates the server
+%%--------------------------------------------------------------------
+init([]) ->
+  inets:start(),
+  ssl:start(),
+  NState = refresh(#state{}),
+  {ok, NState}.
+
+%%--------------------------------------------------------------------
+%% Function: %% handle_call(Request, From, State) -> {reply, Reply, State} |
+%%                                      {reply, Reply, State, Timeout} |
+%%                                      {noreply, State} |
+%%                                      {noreply, State, Timeout} |
+%%                                      {stop, Reason, Reply, State} |
+%%                                      {stop, Reason, State}
+%% Description: Handling call messages
+%%--------------------------------------------------------------------
+handle_call(_Request, _From, State) ->
+  Reply = ok,
+  {reply, Reply, State}.
+
+%%--------------------------------------------------------------------
+%% Function: handle_cast(Msg, State) -> {noreply, State} |
+%%                                      {noreply, State, Timeout} |
+%%                                      {stop, Reason, State}
+%% Description: Handling cast messages
+%%--------------------------------------------------------------------
+handle_cast(_Msg, State) ->
+  {noreply, State}.
+
+%%--------------------------------------------------------------------
+%% Function: handle_info(Info, State) -> {noreply, State} |
+%%                                       {noreply, State, Timeout} |
+%%                                       {stop, Reason, State}
+%% Description: Handling all non call/cast messages
+%%--------------------------------------------------------------------
+handle_info(refresh, State) ->
+	case refresh(State) of
+		{ok, NState} -> {noreply, NState};
+		_ -> {noreply, State}
+	end;
+handle_info(_Info, State) ->
+  {noreply, State}.
+
+%%--------------------------------------------------------------------
+%% Function: terminate(Reason, State) -> void()
+%% Description: This function is called by a gen_server when it is about to
+%% terminate. It should be the opposite of Module:init/1 and do any necessary
+%% cleaning up. When it returns, the gen_server terminates with Reason.
+%% The return value is ignored.
+%%--------------------------------------------------------------------
+terminate(_Reason, _State) ->
+  ok.
+
+%%--------------------------------------------------------------------
+%% Func: code_change(OldVsn, State, Extra) -> {ok, NewState}
+%% Description: Convert process state when code is changed
+%%--------------------------------------------------------------------
+code_change(_OldVsn, State, _Extra) ->
+  {ok, State}.
+
+%%--------------------------------------------------------------------
+%%% Internal functions
+%%--------------------------------------------------------------------
+
+refresh(State) ->
+  Now = os:timestamp(),
+  HNList = httpc:request(?HNURL),
+  erlang:send_after(?FETCHINTERVAL, self(), refresh),
+  NState  = State#state{last_update = Now, hn_list = HNList},
+  io:format("nohn_fetcher: fetched list"),
+  {ok, NState}.
+
+%fetch_item(Item) ->
+	%{Item, {}}.	
