216679916699f65c8bf3e7587819f7063b640683
klObs <jw@klobs.de>
2017-02-05 13:44:03 +0100

    Style aenderungen und cookie comparer

--- src/nohn_handler.erl
+++ src/nohn_handler.erl
@@ -12,10 +12,9 @@
 init(Req0, State) ->
 	Cookies = cowboy_req:parse_cookies(Req0),
 	LastVisitCookie = case  lists:keyfind(<<"last_visit">>, 1, Cookies) of
-		false -> <<"">>;
-		{_, T} -> T
+		false -> 0;
+		{_, T} -> list_to_integer(binary_to_list(T))
 	end,
-	io:format("~p~n",[LastVisitCookie]),
 	Req1 = cowboy_req:set_resp_cookie(<<"last_visit">>, integer_to_list(os:system_time()), Req0),
     Req = cowboy_req:reply(200,
         #{<<"content-type">> => <<"text/html">>},
@@ -27,24 +26,32 @@ init(Req0, State) ->
 gen_nohn_table(LastVisitCookie) ->
 	LastVisitCookie,
 	{ok, HNStatus} = nohn_fetcher:get_status(),
-	[<<"<!DOCTYPE html><html><body><h1>No old hacker news!</h1>\n">>,
+	[<<"<!DOCTYPE html><html>">>,
+			<<"<head><style>">>,
+			<<".hidden { visibility: hidden; background-color:#D46A6A; }">>,
+			<<"</style></head>">>,
+		<<"<body>\n<h1>No old hacker news!</h1>\n">>,
 		<<"<table>\n">>,		
-		gen_nohn_table(1, HNStatus#state.hn_list, HNStatus),
+		gen_nohn_table(1, HNStatus#state.hn_list, LastVisitCookie, HNStatus),
 		<<"</table></body></html>\n">>	
 		].
 
-gen_nohn_table(_ItenNo, [], _HNStatus) ->
+gen_nohn_table(_ItenNo, [], _LastVisitCookie, _HNStatus) ->
 	<<"">>;
-gen_nohn_table(ItenNo, [CurrentItemNo | HNList], HNStatus) ->
+gen_nohn_table(ItenNo, [CurrentItemNo | HNList], LastVisitCookie, HNStatus) ->
 	Item = maps:get(CurrentItemNo, HNStatus#state.item_store, #{}),
 	ItemTitle =  maps:get(<<"title">>, Item, <<"Not yet in itemstore">>),
 	ItemURL =  maps:get(<<"url">>, Item, <<"Not yet in itemstore">>),
 	ItemLink = [<<"<a href=\"">>,ItemURL,<<"\">">>, ItemTitle, <<"</a>">>],
 	ItemScore = maps:get(<<"score">>, Item, <<"Not yet in itemstore">>),
-	[<<"<tr>">>,
+	ItemClass = case LastVisitCookie > maps:get(nohn_timestamp, Item, false) of
+		true -> <<"hidden">>;
+		false -> <<"shown">>
+	end,
+	[<<"<tr class=\"">>,ItemClass,<<"\">">>,
 			<<"<td>">>, list_to_binary(integer_to_list(ItenNo)), <<".</td>">>,
 			<<"<td>">>, ItemLink, <<"</td>">>,
 			<<"<td>Score: ">>, integer_to_list(ItemScore), <<"</td>">>,
 		<<"</tr>">>
 		] 
-		++ gen_nohn_table(ItenNo + 1, HNList, HNStatus).
+		++ gen_nohn_table(ItenNo + 1, HNList, LastVisitCookie, HNStatus).
